f95f5082c1fad8d5e62f70227c576496ebebcf47
legend <Legend94rz@gmail.com>
2018-01-22 12:48:11 +0800

    网络模型。未测试

---
+++ WeChatJumpGame/Model.py
@@ -0,0 +1,132 @@
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Conv2D, Dropout, BatchNormalization, Activation, MaxPooling2D, Reshape, Flatten
+from keras.utils import plot_model
+import numpy as np
+import os
+import cv2
+
+class CoarseModel(object):
+    def __init__(self, **kwargs):
+        self.batchSize = 16
+        self.dataDir = './Data'
+        self.name_list = []
+        self.get_name_list()
+        #==============================
+        self.m = Sequential()
+        self.m.add(Conv2D( 16,(3,3),input_shape = (640,720,3),strides = 2, padding = 'same', activation='relu',bias_initializer = 'constant',kernel_initializer = 'truncated_normal'))
+
+        self.m.add(Conv2D(32,(3,3),padding = 'same',bias_initializer = 'constant',kernel_initializer = 'truncated_normal'))
+        self.m.add(BatchNormalization())
+        self.m.add(Activation('relu'))
+        self.m.add(MaxPooling2D(pool_size = (2,2), strides = (2,2), padding = 'same'))
+
+        self.m.add(Conv2D(64,(5,5),padding = 'same',bias_initializer = 'constant',kernel_initializer = 'truncated_normal'))
+        self.m.add(BatchNormalization())
+        self.m.add(Activation('relu'))
+        self.m.add(MaxPooling2D(pool_size = (2,2), strides = (2,2), padding = 'same'))
+
+        self.m.add(Conv2D(128,(7,7),padding = 'same',bias_initializer = 'constant',kernel_initializer = 'truncated_normal'))
+        self.m.add(BatchNormalization())
+        self.m.add(Activation('relu'))
+        self.m.add(MaxPooling2D(pool_size = (2,2), strides = (2,2), padding = 'same'))
+
+        self.m.add(Conv2D(256,(9,9),padding = 'same',bias_initializer = 'constant',kernel_initializer = 'truncated_normal'))
+        self.m.add(BatchNormalization())
+        self.m.add(Activation('relu'))
+        self.m.add(MaxPooling2D(pool_size = (2,2), strides = (2,2), padding = 'same'))
+
+        self.m.add(Flatten())
+        self.m.add(Dense( 256 ))
+        self.m.add(Dense(2))
+
+        self.m.compile(optimizer='adam',loss = 'mse',metrics = ['accuracy'])
+        plot_model(self.m,'CoarseModel.png',show_shapes = True,show_layer_names = True)
+
+    def get_name_list(self):
+        for i in range(3, 10):
+            dir = os.path.join(self.dataDir, 'exp_%02d' % i)
+            this_name = os.listdir(dir)
+            this_name = [os.path.join(dir, name) for name in this_name]
+            self.name_list = self.name_list + this_name
+        self.name_list_raw = self.name_list
+        self.name_list = filter(lambda name: 'res' in name, self.name_list)
+        self.name_list = list(self.name_list)
+
+        def _name_checker(name):
+            posi = name.index('_res')
+            img_name = name[:posi] + '.png'
+            if img_name in self.name_list_raw:
+                return True
+            else:
+                return False
+
+        self.name_list = list(filter(_name_checker, self.name_list))
+
+    def nextBatch(self):
+        while True:
+            batch_name = np.random.choice(self.name_list, self.batchSize)
+            batch = {}
+            for idx, name in enumerate(batch_name):
+                posi = name.index('_res')
+                img_name = name[:posi] + '.png'
+                x, y = name[name.index('_h_') + 3: name.index('_h_') + 6], name[name.index('_w_') + 3: name.index('_w_') + 6]
+                x, y = int(x), int(y)
+                img = cv2.imread(img_name)
+                img = img[320: -320, :, :]
+                label = np.array([x, y], dtype=np.float32)
+                mask1 = (img[:, :, 0] == 245)
+                mask2 = (img[:, :, 1] == 245)
+                mask3 = (img[:, :, 2] == 245)
+                mask = mask1 * mask2 * mask3
+                img[mask] = img[x - 320 + 10, y + 14, :]
+                if idx == 0:
+                    batch['img'] = img[np.newaxis, :, :, :]
+                    batch['label'] = label.reshape([1, label.shape[0]])
+                else:
+                    img_tmp = img[np.newaxis, :, :, :]
+                    label_tmp = label.reshape((1, label.shape[0]))
+                    batch['img'] = np.concatenate((batch['img'], img_tmp), axis=0)
+                    batch['label'] = np.concatenate((batch['label'], label_tmp), axis=0)
+            #yield (batch['img'],batch['label'])
+            return (batch['img'],batch['label'])
+
+    def train(self):
+        #self.m.fit_generator(self.nextBatch(), epochs = 100000, steps_per_epoch = 16,verbose = 2)
+        x,y = self.nextBatch()
+        self.m.train_on_batch(x,y)
+        self.m.save('fineModel.h5')
+
+    def predict(self,X):
+        return self.m.predict(X)
+    
+class FineMode(object):
+    def __init__(self, **kwargs):
+        self.m = Sequential()
+        self.m.add(Conv2D(16,(3,3),input_shape = (320,320,3),strides = 2, padding = 'same',activation = 'relu',bias_initializer = 'constant', kernel_initializer = 'truncated_normal'))
+        self.m.add(Conv2D(64,(3,3),padding = 'same',bians_initializer = 'constant',kernel_initilizer = 'truncated_normal'))
+        self.m.add(MaxPooling2D(pool_size = (2,2),strides = (2,2),padding = 'same'))
+
+        self.m.add(Conv2D(128,(5,5),padding = 'same',bians_initializer = 'constant',kernel_initilizer = 'truncated_normal'))
+        self.m.add(MaxPooling2D(pool_size = (2,2),strides = (2,2),padding = 'same'))
+
+        self.m.add(Conv2D(256,(7,7),padding = 'same',bians_initializer = 'constant',kernel_initilizer = 'truncated_normal'))
+        self.m.add(MaxPooling2D(pool_size = (2,2),strides = (2,2),padding = 'same'))
+
+        self.m.add(Conv2D(512,(9,9),padding = 'same',bians_initializer = 'constant',kernel_initilizer = 'truncated_normal'))
+        self.m.add(MaxPooling2D(pool_size = (2,2),strides = (2,2),padding = 'same'))
+
+        self.m.add(Flatten())
+        self.m.add(Dense(512))
+        self.m.add(Dense(2))
+
+        self.m.compile(optimizer = 'adam',loss = 'mse',metrics = ['accruacy'])
+        plot_model(self.m,'FineModel.png',show_shapes = True,show_layer_names = True)
+    def predict(self,X):
+        return self.m.predict(X)
+    def train(self):
+        pass
+
+if __name__=="__main__":
+    m = CoarseModel()
+    d = m.nextBatch()
+    m.train()
--- WeChatJumpGame/WeChatJumpGame.py
+++ WeChatJumpGame/WeChatJumpGame.py
@@ -1,4 +1,14 @@
 import os
+import matplotlib.pyplot as plt
+import subprocess
+import cv2
+import numpy as np
+from Model import CoarseModel
+
+SCREENSHOT_WAY = 2
+capFileName = 'cap.png'
+chessFileName = 'chess.png'
+
 def pull_screenshot():
     """
     获取屏幕截图，目前有 0 1 2 3 四种方法，未来添加新的平台监测方法时，
@@ -6,17 +16,35 @@ def pull_screenshot():
     """
     global SCREENSHOT_WAY
     if 1 <= SCREENSHOT_WAY <= 3:
-        process = subprocess.Popen(
-            'adb shell screencap -p',
-            shell=True, stdout=subprocess.PIPE)
+        process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
         binary_screenshot = process.stdout.read()
-        #if SCREENSHOT_WAY == 2:
-        #    binary_screenshot = binary_screenshot.replace(b'\r\n', b'\n')
-        #elif SCREENSHOT_WAY == 1:
-        #    binary_screenshot = binary_screenshot.replace(b'\r\r\n', b'\n')
-        f = open('autojump.png', 'wb')
-        f.write(binary_screenshot)
-        f.close()
+        binary_screenshot = binary_screenshot.replace(b'\r\n', b'\n')
+        with open(capFileName, 'wb') as f:
+            f.write(binary_screenshot)
     elif SCREENSHOT_WAY == 0:
-        os.system('adb shell screencap -p /sdcard/autojump.png')
-        os.system('adb pull /sdcard/autojump.png .')
+        os.system('adb shell screencap -p /sdcard/'+capFileName)
+        os.system('adb pull /sdcard/'+capFileName+' .')
+
+def findStartLocation(src,template):
+    result = cv2.matchTemplate(src,template,cv2.TM_CCOEFF)
+    loc = cv2.minMaxLoc(result)
+    return np.array([ loc[-1], (template.shape[1],template.shape[0]) ])
+
+def findTargetLocation(src):
+    return np.array([0,0])
+
+if __name__=="__main__":
+    '''
+    pull_screenshot()
+    template = cv2.imread(chessFileName)
+    src = cv2.imread(capFileName)
+    p = findStartLocation(src,template)
+
+    print('find: %r,%r'%(p[0,0],p[0,1]))
+    cv2.rectangle(src, tuple( p[0,:] ), tuple( np.sum(p,axis = 0) ),(255,0,0),thickness = 3)
+
+    cv2.namedWindow('1',cv2.WINDOW_NORMAL)
+    cv2.imshow('1',src)
+    cv2.waitKey(0)
+    cv2.destroyWindow('1')
+    '''
